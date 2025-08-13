d9bc4fa2bfb51e60c26b3bc63ab3c49d7999f491
See Hong <seehong620@gmail.com>
2025-08-13 18:28:39 +0800

    refactor(crud): Implement full file-based CRUD for students and teachers

--- PST2/pst2_main.py
+++ PST2/pst2_main.py
@@ -1,9 +1,10 @@
 import json
 from json import JSONDecodeError
 
+
+# JSON data management
 app_data = {}
 DATA_FILE = "msms.json"
-
 def load_data(path):
     """Loads all application data from a JSON file."""
     global app_data
@@ -28,3 +29,67 @@ def save_data(path):
     with open(file=path, mode="w") as file:
         json.dump(app_data, fp=file, indent=4)
     print("Data saved successfully.")
+
+# Core Helper Functions
+def add_teacher(name, specialty):
+    """Adds a teacher to the application data."""
+
+    new_teacher = {
+        "name": name,
+        "id": app_data["next_teacher_id"],
+        "specialty": specialty,
+    }
+    app_data["teachers"].append(new_teacher)
+    app_data["next_teacher_id"] += 1
+
+def update_teacher(teacher_id, **kwargs):
+    """Updates an existing teachers information."""
+    for teacher in app_data["teachers"]:
+        if teacher["id"] == teacher_id:
+            # Executes if user has provided items to change.
+            if kwargs:
+                teacher.update(kwargs)
+                print(f"Teacher ID {teacher_id} updated.")
+                print("-" * 20)
+                for data in kwargs:
+                    print(f"{data.capitalize()} : {kwargs[data]}")
+                print("-" * 20)
+                return
+            else:
+                print(f"No data for teacher ID {teacher_id} has been changed")
+    print(f"Teacher ID {teacher_id} not found.")
+
+def remove_teacher(teacher_id):
+    """Removes an existing teacher from the application data."""
+    for teacher in app_data["teachers"]:
+        if teacher["id"] == teacher_id:
+            app_data["teachers"].remove(teacher)
+            print(f"Teacher ID {teacher_id} has been removed.")
+            return
+    print(f"Teacher ID {teacher_id} not found.")
+
+def update_student(student_id, **kwargs):
+    """Updates an existing students information."""
+    for student in app_data["teachers"]:
+        if student["id"] == student_id:
+            # Executes if user has provided items to change.
+            if kwargs:
+                student.update(kwargs)
+                print(f"Student ID {student_id} updated.")
+                print("-" * 20)
+                for data in kwargs:
+                    print(f"{data.capitalize()} : {kwargs[data]}")
+                print("-" * 20)
+                return
+            else:
+                print(f"No data for student ID {student_id} has been changed")
+    print(f"Student ID {student_id} not found.")
+
+def remove_student(student_id):
+    """Removes an existing student from the application data."""
+    for student in app_data["teachers"]:
+        if student["id"] == student_id:
+            app_data["students"].remove(student)
+            print(f"Student ID {student_id} has been removed.")
+            return
+    print(f"Student ID {student_id} not found.")
